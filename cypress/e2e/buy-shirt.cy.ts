54cd10a355129a37d41122adb119f9c867c94a1e
Sanberqui18 <san.be.qui18@gmail.com>
2023-12-05 16:47:56 -0500

    Fix BDD with Product Page POM

--- cypress/e2e/buy-shirt.cy.ts
+++ cypress/e2e/buy-shirt.cy.ts
@@ -13,40 +13,48 @@ describe("Buy a product workflow", () => {
       productPage.displayContainer();
     });
 
-    describe("When the user adds the Sauce Labs Backpack to the cart ", () => {
+    describe("When the user adds the Sauce Labs Backpack to the cart", () => {
       beforeEach( () => { 
           productPage.addItem();
-          productPage.verifyTitle("Sauce Labs Backpack");
-          productPage.verifyPrice("$29.99");
-          productPage.goToShoppingCart();
+      });
+      
+      it("Then the price and names for that product are correct", () => {
+        productPage.verifyTitle("Sauce Labs Backpack");
+        productPage.verifyPrice("$29.99");
       });
 
-      describe("And the user goes to the checkout page and fills out the required information and continues", () => {
+      describe("And the user clicks on the shopping cart icon", () => {
         beforeEach(() => {
-          cy.get("#checkout").click();   
-          cy.get("#first-name").type("Cypress");
-          cy.get("#last-name").type("Workshop");
-          cy.get("#postal-code").type("00000");    
-          cy.get("#continue").click(); 
+          productPage.goToShoppingCart();
         });
 
-        describe("And the user checks the summary and finish the order", () => {
-          it("Then the data summary should be displayed", () => {
-            cy.get(".cart_quantity").should("have.text", 1);
-            cy.get(".inventory_item_name").should("have.text", "Sauce Labs Backpack");
-            cy.get(".inventory_item_price").should("have.text", "$29.99");
-            cy.get(".summary_subtotal_label").should("contain.text", "$29.99");
-            cy.get(".summary_tax_label").should("contain.text", "$2.40");
-            cy.get(".summary_total_label").should("contain.text", "$32.39");
+        describe("And the user goes to the checkout page and fills out the required information and continues", () => {
+          beforeEach(() => {
+            cy.get("#checkout").click();   
+            cy.get("#first-name").type("Cypress");
+            cy.get("#last-name").type("Workshop");
+            cy.get("#postal-code").type("00000");    
+            cy.get("#continue").click(); 
           });
-          
-          describe("And the user finishes the order", () => {
-            beforeEach(() => {
-              cy.get("#finish").click();
+  
+          describe("And the user checks the summary and finish the order", () => {
+            it("Then the data summary should be displayed", () => {
+              cy.get(".cart_quantity").should("have.text", 1);
+              cy.get(".inventory_item_name").should("have.text", "Sauce Labs Backpack");
+              cy.get(".inventory_item_price").should("have.text", "$29.99");
+              cy.get(".summary_subtotal_label").should("contain.text", "$29.99");
+              cy.get(".summary_tax_label").should("contain.text", "$2.40");
+              cy.get(".summary_total_label").should("contain.text", "$32.39");
             });
             
-            it("Then the user should get the completed order message", () => {
-              cy.get(".complete-header").should("have.text", "Thank you for your order!");   
+            describe("And the user finishes the order", () => {
+              beforeEach(() => {
+                cy.get("#finish").click();
+              });
+              
+              it("Then the user should get the completed order message", () => {
+                cy.get(".complete-header").should("have.text", "Thank you for your order!");   
+              });
             });
           });
         });
