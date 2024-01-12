40f23cd4d0725494bb4ba68e754a4548fb1723d8
Sanberqui18 <san.be.qui18@gmail.com>
2024-01-12 15:43:24 -0500

    Fix BDD and TS version

--- cypress/e2e/download.file.cy.ts
+++ cypress/e2e/download.file.cy.ts
@@ -5,15 +5,15 @@ import { DownloadPage } from "../pages";
 const downloadPage = new DownloadPage();
 
 describe("Download File", () => {
-  describe("Given the user is in teh Download Site ", () => {
+  describe("Given the user is in the Download Site ", () => {
     beforeEach(() => {
       downloadPage.goToDownloadSite();
     });
 
     describe("When the user downloads a file by clicking on the Download button", () => {
-      it("Then the file should be downloaded and it is correct", () => {
+      it("Then the file should be downloaded", () => { //pending to add visual testing
         downloadPage.downloadImage();
-        downloadPage.getDownloadedFile().should("exist");
+        downloadPage.getDownloadedFile().should("exist")
       });
     });
   });
